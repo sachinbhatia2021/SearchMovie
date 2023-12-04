f66ed46536c8434f0a923cdfa08e90649df9f94e
sachinbhatia2021 <152883878+sachinbhatia2021@users.noreply.github.com>
2023-12-04 20:00:24 +0530

    Add files via upload

---
+++ love.js
@@ -0,0 +1,10 @@
+function yesLoveu(){
+
+    document.getElementById('answer').innerHTML=`I Love u too Baby <img src="https://freepngimg.com/download/icon/1000582-kiss-emoji-free-photo-icon.png" height="100px" alt=""> `
+}
+
+
+
+
+
+
